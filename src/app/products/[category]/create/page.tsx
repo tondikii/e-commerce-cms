cdfb85d1ec606b781d2d62330fdfa65364c8bfa9
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2025-06-30 16:16:15 +0700

    move baseURL to env

--- src/app/products/[category]/create/page.tsx
+++ src/app/products/[category]/create/page.tsx
@@ -8,6 +8,7 @@ const getSizes = async () => {
     const {data} = await api.get("size");
     return data;
   } catch (err) {
+    console.log("ERR sizes", err);
     return [];
   }
 };
@@ -17,6 +18,7 @@ const getColors = async () => {
     const {data} = await api.get("color");
     return data;
   } catch (err) {
+    console.log("ERR colors", err);
     return [];
   }
 };
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api/",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL_API,
   timeout: 325000,
   headers: {"X-Requested-With": "XMLHttpRequest"},
 });
